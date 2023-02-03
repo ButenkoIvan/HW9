561c8101b54bc6838e18b97566478757de4bf5a4
Іван <butenkoivan898@gmail.com>
2023-02-03 10:50:52 +0200

    test

--- src/main/java/Timezone.java
+++ src/main/java/Timezone.java
@@ -1,13 +1,20 @@
-import org.jetbrains.annotations.NotNull;
-
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import java.util.stream.Stream;
 
 public class Timezone {
-    public String parseTimeZone(@NotNull HttpServletRequest request) {
+    public String parseTimeZone(HttpServletRequest request) {
+        String standardTZ = "UTC";
+        String result = (request.getCookies() == null) ? standardTZ :
+                Stream.of(request.getCookies())
+                        .filter(c -> "lastTimezone".equals(c.getName()))
+                        .map(Cookie::getValue)
+                        .findAny().orElseGet(() -> standardTZ);
+
         if (request.getParameterMap().containsKey("timezone")) {
             return (request.getParameter("timezone").replace(" ", "+").length() < 1) ?
-                    "UTC" : request.getParameter("timezone").replace(" ", "+").toUpperCase();
+                    result : request.getParameter("timezone").replace(" ", "+").toUpperCase();
         }
-        return "UTC";
+        return result;
     }
 }
