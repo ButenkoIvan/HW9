72c7735f9c1da8803b6b854cd9717c24e1ca6f39
Іван <butenkoivan898@gmail.com>
2023-02-10 16:27:28 +0200

    test

--- src/main/java/TimezoneValidateFilter.java
+++ src/main/java/TimezoneValidateFilter.java
@@ -7,33 +7,27 @@ import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.DateTimeException;
-import java.time.ZoneId;
+//import java.time.DateTimeException;
+//import java.time.ZoneId;
+//import java.util.TimeZone;
 
-@WebFilter(value = "/time")
+@WebFilter(value = "/*")
 public class TimezoneValidateFilter extends HttpFilter {
     private final Timezone timeZoneUtil = new Timezone();
 
     @Override
-    protected void doFilter(HttpServletRequest req,
-                            HttpServletResponse resp,
-                            FilterChain chain) throws IOException, ServletException {
-        boolean validZoneId = true;
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
 
-        try {
-            ZoneId.of(timeZoneUtil.parseTimeZone(req));
-        } catch (DateTimeException ex) {
-            validZoneId = false;
+        if (!req.getParameterMap().containsKey("timezone")) {
+            chain.doFilter(req, res);
         }
 
-        if (validZoneId) {
-            chain.doFilter(req, resp);
-        } else {
-            resp.setStatus(400);
-
-            resp.setContentType("application/json");
-            resp.getWriter().write("Invalid timezone");
-            resp.getWriter().close();
+        if (req.getParameterMap().containsKey("timezone")) {
+            String utc = req.getParameter("timezone").replace(" ", "+");
+          //  if (new TimeZone().timeZone().contains(utc)) {
+                chain.doFilter(req, res);
+            } else {
+                res.sendError(400, "Invalid timezone");
+            }
         }
     }
-}
